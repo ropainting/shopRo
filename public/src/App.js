01703c27d147906fce1763a324ec061d70c98f2a
ropainting <rartfr@gmail.com>
2025-04-21 21:01:31 +0800

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>🖌️ Ro_Safi Creations</h1>
+      <p>Beautiful handmade paintings, bracelets, bookmarks, and more!</p>
+      <p>Online shop coming soon…</p>
+    </div>
+  );
+};
+
+export default App;
